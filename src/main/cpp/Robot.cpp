e8dcbae244fce1ede9201bb0511717a2ee1c6a89
MrRSquared <jrothritchie@hdsd.org>
2021-08-17 08:58:29 -0400

    Initial commit

---
+++ src/main/cpp/Robot.cpp
@@ -0,0 +1,60 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#include <frc/Joystick.h>
+#include <frc/PWMVictorSPX.h>
+#include <frc/TimedRobot.h>
+#include <frc/drive/MecanumDrive.h>
+#include <studica/TitanQuad.h>
+
+/**
+ * This is a demo program showing how to use Mecanum control with the
+ * MecanumDrive class. If using the VMX-Pi, do not forget to add the vendor libraries
+ * http://dev.studica.com/releases/2020/Studica.json
+ */
+class Robot : public frc::TimedRobot {
+ public:
+  void RobotInit() override {
+    // Invert the left side motors. You may need to change or remove this to
+    // match your robot.
+    m_frontLeft.SetInverted(true);
+    m_rearLeft.SetInverted(true);
+  }
+
+  void TeleopInit() override {
+    //This is a wonderful debug tool. Print to the console.
+    wpi::outs() << "Entering Teleop...";
+  }
+
+  void TeleopPeriodic() override {
+    /* Use the joystick X axis for lateral movement, Y axis for forward
+     * movement, and Z axis for rotation.
+     */
+    m_robotDrive.DriveCartesian(m_stick.GetX(), m_stick.GetY(), m_stick.GetZ());
+  }
+
+ private:
+  static constexpr int kFrontLeftChannel = 0;
+  static constexpr int kRearLeftChannel = 1;
+  static constexpr int kFrontRightChannel = 2;
+  static constexpr int kRearRightChannel = 3;
+
+  static constexpr int kJoystickChannel = 0;
+
+  frc::PWMVictorSPX m_frontLeft{kFrontLeftChannel};
+  frc::PWMVictorSPX m_rearLeft{kRearLeftChannel};
+  frc::PWMVictorSPX m_frontRight{kFrontRightChannel};
+  frc::PWMVictorSPX m_rearRight{kRearRightChannel};
+  frc::MecanumDrive m_robotDrive{m_frontLeft, m_rearLeft, m_frontRight,
+                                 m_rearRight};
+
+  frc::Joystick m_stick{kJoystickChannel};
+};
+
+#ifndef RUNNING_FRC_TESTS
+int main() { return frc::StartRobot<Robot>(); }
+#endif
